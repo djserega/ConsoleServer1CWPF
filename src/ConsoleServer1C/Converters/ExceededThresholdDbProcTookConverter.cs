0d658596de19a5d2d22115a528a615fc77c5b58f
djserega <galyuk.sergey@gmail.com>
2018-11-08 12:02:15 +0200

    added: converter ExceededThresholdDbProcTookConverter

---
+++ src/ConsoleServer1C/Converters/ExceededThresholdDbProcTookConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ConsoleServer1C.Converters
+{
+    public class ExceededThresholdDbProcTookConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float floatValue = ToFloat(value);
+
+            if (floatValue >= 30)
+                return new SolidColorBrush(Colors.Red);
+            else if (floatValue >= 20)
+                return new SolidColorBrush(Colors.LimeGreen);
+            else if (floatValue >= 10)
+                return new SolidColorBrush(Colors.Yellow);
+            else
+                return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        private float ToFloat(object value)
+        {
+            return System.Convert.ToSingle(value);
+        }
+    }
+}
