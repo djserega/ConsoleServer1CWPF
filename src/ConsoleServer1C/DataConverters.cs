579de5b18862fb231604d928906599c45cefe88d
djserega <galyuk.sergey@gmail.com>
2018-11-08 10:31:07 +0200

    added: class converter value
    Convert byte (long, ulong) 2 string (B, KB, MB, GB, TB, PB, EB)

---
+++ src/ConsoleServer1C/DataConverters.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleServer1C
+{
+    internal class DataConverters
+    {
+        internal static string BytesToString(ulong bytes)
+        {
+            if (bytes == 0)
+                return "";
+
+            else if (bytes < 1024)
+                return (bytes).ToString("F0") + " B";
+
+            else if ((bytes >> 10) < 1024)
+                return (bytes / (float)1024).ToString("F1") + " KB";
+
+            else if ((bytes >> 20) < 1024)
+                return ((bytes >> 10) / (float)1024).ToString("F1") + " MB";
+
+            else if ((bytes >> 30) < 1024)
+                return ((bytes >> 20) / (float)1024).ToString("F1") + " GB";
+
+            else if ((bytes >> 40) < 1024)
+                return ((bytes >> 30) / (float)1024).ToString("F1") + " TB";
+
+            else if ((bytes >> 50) < 1024)
+                return ((bytes >> 40) / (float)1024).ToString("F1") + " PB";
+
+            else
+                return ((bytes >> 50) / (float)1024).ToString("F0") + " EB";
+        }
+
+        internal static string BytesToString(long bytes)
+        {
+            if (bytes == 0)
+                return "";
+
+            if (bytes > 0)
+            {
+                if (bytes < 1024)
+                    return (bytes).ToString("F0") + " B";
+
+                else if ((bytes >> 10) < 1024)
+                    return (bytes / (float)1024).ToString("F1") + " KB";
+
+                else if ((bytes >> 20) < 1024)
+                    return ((bytes >> 10) / (float)1024).ToString("F1") + " MB";
+
+                else if ((bytes >> 30) < 1024)
+                    return ((bytes >> 20) / (float)1024).ToString("F1") + " GB";
+
+                else if ((bytes >> 40) < 1024)
+                    return ((bytes >> 30) / (float)1024).ToString("F1") + " TB";
+
+                else if ((bytes >> 50) < 1024)
+                    return ((bytes >> 40) / (float)1024).ToString("F1") + " PB";
+
+                else
+                    return ((bytes >> 50) / (float)1024).ToString("F0") + " EB";
+            }
+            else
+            {
+                if (bytes > -1024)
+                    return (bytes).ToString("F0") + " B";
+
+                else if ((bytes >> 10) > -1024)
+                    return (bytes / (float)1024).ToString("F1") + " KB";
+
+                else if ((bytes >> 20) > -1024)
+                    return ((bytes >> 10) / (float)1024).ToString("F1") + " MB";
+
+                else if ((bytes >> 30) > -1024)
+                    return ((bytes >> 20) / (float)1024).ToString("F1") + " GB";
+
+                else if ((bytes >> 40) > -1024)
+                    return ((bytes >> 30) / (float)1024).ToString("F1") + " TB";
+
+                else if ((bytes >> 50) > -1024)
+                    return ((bytes >> 40) / (float)1024).ToString("F1") + " PB";
+
+                else
+                    return ((bytes >> 50) / (float)1024).ToString("F0") + " EB";
+
+            }
+        }
+    }
+}
