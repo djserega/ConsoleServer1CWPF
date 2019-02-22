d130bf596f9e5553250b2f2d676beb8a19b264b3
djserega <galyuk.sergey@gmail.com>
2019-02-22 21:19:37 +0200

    added: ElementsFormConverter

---
+++ src/ConsoleServer1C/Converters/ElementsFormConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+
+namespace ConsoleServer1C.Converters
+{
+    public class ElementsFormConverter : MarkupExtension, IValueConverter
+    {
+        private static ElementsFormConverter _instance;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double result = System.Convert.ToDouble(value) - System.Convert.ToDouble(parameter);
+            return result < 0 ? 0 : result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double result = System.Convert.ToDouble(value) - System.Convert.ToDouble(parameter);
+            return result < 0 ? 0 : result;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ?? (_instance = new ElementsFormConverter());
+        }
+    }
+}
