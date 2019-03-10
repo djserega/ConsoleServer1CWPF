53ea0c17d98f974bc746b3f89e21c8d0a4de51ce
djserega <galyuk.sergey@gmail.com>
2019-03-10 14:07:28 +0200

    added summary list without access

--- src/ConsoleServer1C/InfoBaseWithoutAccess.cs
+++ src/ConsoleServer1C/InfoBaseWithoutAccess.cs
@@ -2,13 +2,29 @@
 
 namespace ConsoleServer1C
 {
+    /// <summary>
+    /// Класс имен баз данных без доступа к информации
+    /// </summary>
     public static class InfoBaseWithoutAccess
     {
+        /// <summary>
+        /// Локер добавления
+        /// </summary>
         private static readonly object _lockerAdd = new object();
+        /// <summary>
+        /// Локер проверки наличия
+        /// </summary>
         private static readonly object _lockerContains = new object();
 
+        /// <summary>
+        /// Список имен баз данных
+        /// </summary>
         public static List<string> InfoBasesName { get; set; } = new List<string>();
 
+        /// <summary>
+        /// Добавление базы данных
+        /// </summary>
+        /// <param name="infoBaseName"></param>
         public static void AddInfoBase(string infoBaseName)
         {
             lock (_lockerAdd)
@@ -18,6 +34,11 @@ namespace ConsoleServer1C
             }
         }
 
+        /// <summary>
+        /// Проверка наличия имени базы данных в списке
+        /// </summary>
+        /// <param name="infoBaseName"></param>
+        /// <returns></returns>
         public static bool InfoBaseContains(string infoBaseName)
         {
             lock (_lockerContains)
