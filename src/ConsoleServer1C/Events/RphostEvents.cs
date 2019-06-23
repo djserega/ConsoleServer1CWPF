f109e1a7fce50cea5e9d25c56f6af8a8e865433c
djserega <galyuk.sergey@gmail.com>
2019-06-23 16:01:00 +0300

    added event rphost

---
+++ src/ConsoleServer1C/Events/RphostEvents.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleServer1C.Events
+{
+    public delegate void NewObject(Models.RphostObject obj);
+    public class RphostEvents
+    {
+        public static event NewObject NewObject;
+
+        public static void Created(Models.RphostObject obj) => NewObject?.Invoke(obj);
+    }
+}
