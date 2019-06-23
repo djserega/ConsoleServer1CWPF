31ae0455470e27aa222285ccee951cde383dd61a
djserega <galyuk.sergey@gmail.com>
2019-06-23 16:00:54 +0300

    added model rphost

---
+++ src/ConsoleServer1C/Models/RphostObject.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleServer1C.Models
+{
+    public class RphostObject
+    {
+        public int PID { get; set; }
+        public string SizeText { get; set; }
+        public int Size { get; set; }
+
+        public bool Fill(string data)
+        {
+            bool result = false;
+
+            if (data?.StartsWith("rphost.exe") ?? false)
+            {
+                string fillData = data.Substring(10).TrimStart();
+
+                string pid = string.Empty;
+                foreach (char charData in fillData)
+                {
+                    if (charData == ' ')
+                        break;
+
+                    pid += charData;
+                }
+
+                PID = int.Parse(pid);
+                SizeText = fillData
+                    .Substring(pid.Length)
+                    .TrimStart()
+                    .Substring(1)
+                    .TrimStart();
+                Size = int.Parse(SizeText.Substring(0, SizeText.Length - 2).Replace(" ", "").Trim());
+
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
